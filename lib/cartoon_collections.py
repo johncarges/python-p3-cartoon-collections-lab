90689243b640f49fbbf76977ea5b7f982b24ddf2
John Carges <johncarges@gmail.com>
2023-08-21 16:52:47 -0400

    passes all tests with four super useful functions

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,22 @@
-def roll_call_dwarves():
+def roll_call_dwarves(dwarves):
+    for index, name in enumerate(dwarves):
+        print(f"{index+1}. {name}")
     pass
 
-def summon_captain_planet():
+def summon_captain_planet(planeteers):
+    return [f"{planeteer.capitalize()}!" for planeteer in planeteers]
     pass
 
-def long_planeteer_calls():
+def long_planeteer_calls(planeteers):
+    return any([len(planeteer)>4 for planeteer in planeteers])
     pass
 
-def find_the_cheese():
+def find_the_cheese(snacks):
+    cheeses = ["cheddar", "gouda", "camembert"]
+    for snack in snacks:
+        if snack in cheeses:
+            return snack
+    
+    return None
+
     pass
